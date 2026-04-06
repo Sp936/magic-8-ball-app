e9fbbac26321c98a3d9ea6364fe8e2235812f36f
Sp936 <sphipps936@gmail.com>
2026-04-06 21:31:42 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,25 @@
+const CACHE_NAME = "dopamine-ball-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
 self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
-self.addEventListener("fetch", () => {});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
